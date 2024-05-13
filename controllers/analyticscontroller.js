c275894c5e8a0956c602088135b3d465f86d837b
Abhinav Goel <abhinavgo1000@gmail.com>
2024-05-13 17:40:51 -0400

    controller functions to fetch data by id, update and delete added

--- controllers/analyticscontroller.js
+++ controllers/analyticscontroller.js
@@ -41,3 +41,85 @@ exports.enterData = (req, res, next) => {
         next(err);
     });
 };
+
+exports.fetchDataById = (req, res, next) => {
+    const dataId = req.params.dataId;
+    Analytics.findById(dataId)
+    .then((data) => {
+        if (!data) {
+            const error = new Error('Could not find the data');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).send(data);
+    })
+    .catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.updateData = (req, res, next) => {
+    const dataId = req.params.dataId;
+    const usage = req.body.usage;
+    const traffic = req.body.traffic;
+    const dataTransfer = req.body.dataTransfer;
+    const coverage = req.body.coverage
+    const timeLapsed = req.body.timeLapsed;
+    const populationShare = req.body.populationShare;
+    Analytics.findById(dataId)
+    .then((data) => {
+        if (!data) {
+            const error = new Error('Could not find the data');
+            error.statusCode = 404;
+            throw error;
+        }
+        data.usage = usage;
+        data.traffic = traffic;
+        data.dataTransfer = dataTransfer;
+        data.coverage = coverage;
+        data.timeLapsed = timeLapsed;
+        data.populationShare = populationShare;
+        return data.save();
+    })
+    .then((data) => {
+        res.status(200).json({
+            message: 'Data updated successfully',
+            data: data
+        });
+    })
+    .catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.deleteData = (req, res, next) => {
+    const dataId = req.params.dataId;
+    Analytics.findById(dataId)
+    .then((data) => {
+        if (!data) {
+            const error = new Error('Could not find the data');
+            error.statusCode = 404;
+            throw error;
+        }
+        return Analytics.findByIdAndDelete(dataId);
+    })
+    .then((result) => {
+        console.log(result);
+        res.status(200).json({
+            message: 'Data deleted',
+            data: data
+        });
+    })
+    .catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
--- routes/analyticsroutes.js
+++ routes/analyticsroutes.js
@@ -8,4 +8,10 @@ router.get('/fetch-data', analyticsController.fetchData);
 
 router.post('/enter-data', analyticsController.enterData);
 
+router.get('/fetch-data/:dataId', analyticsController.fetchDataById);
+
+router.put('/update-data/:dataId', analyticsController.updateData);
+
+router.delete('/delete-data/:dataId', analyticsController.deleteData);
+
 module.exports = router;
